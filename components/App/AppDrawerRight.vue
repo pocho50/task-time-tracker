7559567541c67df350f4b604896d082d784096a5
Jose Luis <cabrera@inkuba.com>
2025-04-08 22:37:26 +0200

    feat: Drawer right

---
+++ components/App/AppDrawerRight.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const open = defineModel<boolean>();
+
+const closeDrawer = () => {
+  open.value = false;
+};
+</script>
+
+<template>
+  <Teleport to="body">
+    <!-- Overlay -->
+    <div
+      v-if="open"
+      @click="closeDrawer"
+      class="fixed inset-0 bg-black/30 transition-opacity duration-300 ease-in-out z-40"
+    />
+
+    <!-- Drawer -->
+    <aside
+      class="fixed top-0 right-0 h-full w-full sm:w-[300px] lg:w-[450px] bg-base-100 shadow-lg transition-transform duration-300 ease-in-out z-50"
+      :class="open ? 'translate-x-0' : 'translate-x-full'"
+    >
+      <div class="p-4 h-full overflow-y-auto">
+        <slot></slot>
+      </div>
+    </aside>
+  </Teleport>
+</template>
