e67c42d79a4c8a813616343455fa88b80a89e3c5
Jose Luis <cabrera@inkuba.com>
2025-04-19 12:36:12 +0200

    feat: add layer structure

---
+++ layers/projects/pages/projects/index.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { useRouteQuery } from "@vueuse/router";
+
+const page = useRouteQuery("page", 1, { transform: Number });
+
+const { data, refresh } = await useFetch("/api/projects", {
+  query: { page },
+  watch: false,
+});
+
+watch(page, () => {
+  refresh();
+});
+
+const projects = computed(() => data.value?.data);
+</script>
+<template>
+  <section class="py-12 px-4 bg-base-200">
+    <AppTitle text="Projects" />
+
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6"
+      v-if="projects"
+    >
+      <article
+        v-for="project in projects"
+        :key="project.id"
+        class="card bg-base-100 shadow-md hover:shadow-2xl group relative overflow-hidden transition-transform duration-300 ease-in-out hover:scale-[1.03]"
+      >
+        <AppCardAction :actions="['edit', 'remove']" />
+        <div class="card-body">
+          <h2 class="card-title">
+            {{ project.name }}
+          </h2>
+          <p class="text-base-content/80 text-sm mt-1 line-clamp-3">
+            {{ project.description }}
+          </p>
+          <button class="btn btn-default group-hover:btn-primary">
+            Select project
+          </button>
+        </div>
+      </article>
+    </div>
+    <AppPagination
+      v-if="data?.pagination"
+      :page="page"
+      :totalPages="data?.pagination.pageCount"
+      @@prev="page--"
+      @@next="page++"
+    />
+  </section>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  routeRules: {
+    "/": { redirect: "/projects" },
+  },
   devtools: { enabled: true },
   app: {
     head: {
