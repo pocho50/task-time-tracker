99a3257a9bb6d7c50ea7477c86a33eec510efe01
Jose Luis <cabrera@inkuba.com>
2026-01-03 14:44:46 +0100

    refactor: remove users middleware and reorganize Playwright test projects for better parallelization

--- layers/users/middleware/users.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  if (to.path === '/users') {
-    const { userIsAllowedToWrite } = useUser();
-    if (!userIsAllowedToWrite(ALL_ENTITIES.USERS)) {
-      return navigateTo('/settings');
-    }
-  }
-});
